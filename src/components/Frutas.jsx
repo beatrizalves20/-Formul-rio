3bf3899de86c57b9972409114e401f8f6cff05d3
Beatriz Alves <89867185+beatrizalves20@users.noreply.github.com>
2022-12-19 13:25:19 -0300

    Add files via upload

---
+++ src/components/Frutas.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+
+const Frutas = () => {
+  const itens = ['maçã', 'laranja', 'kiwi', 'pitaya', 'mirtilo'];
+
+  const [quantidade, setQuantidade] = useState(0);
+
+  let [frutas, setFrutas] = useState(itens);
+
+  let [novaFruta, setNovaFruta] = useState('');
+
+  function handleClickIncrementar(event) {
+    setQuantidade((valor) => {
+      return valor + 1;
+    });
+  }
+
+  const handleClickAdicionar = (event) => {
+    setFrutas([...frutas, novaFruta]);
+  };
+
+  const handleChange = function (event) {
+    setNovaFruta(event.target.value);
+  };
+
+  return (
+    <main>
+      Quantidade: {quantidade}
+      <button onClick={handleClickIncrementar}>Incrementar</button>
+      <ol>
+        {frutas.map((fruta) => {
+          return <li>{fruta}</li>;
+        })}
+      </ol>
+      <form>
+        <label>Fruta:</label>
+        <input type="text" value={novaFruta} onChange={handleChange}></input>
+      </form>
+      <button onClick={handleClickAdicionar}>Adicionar</button>
+    </main>
+  );
+};
+
+export default Frutas;
